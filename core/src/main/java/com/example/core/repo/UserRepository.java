51d16c4eadf35ae0f88af4bdf26d85a0f0973aed
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-08-03 17:47:24 +0900

    core: user repository

---
+++ core/src/main/java/com/example/core/repo/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.core.repo;
+
+import com.example.core.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
