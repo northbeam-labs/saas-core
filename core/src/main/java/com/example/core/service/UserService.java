7ea849903696b938a0c713700bfbecc4463c087d
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-08-05 18:19:29 +0900

    core: user service

---
+++ core/src/main/java/com/example/core/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.core.service;
+
+import com.example.core.domain.User;
+import com.example.core.repo.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository repo;
+    public UserService(UserRepository repo) { this.repo = repo; }
+    public List<User> all() { return repo.findAll(); }
+    public User get(Long id) { return repo.findById(id).orElseThrow(); }
+    public User save(User e) { return repo.save(e); }
+}
