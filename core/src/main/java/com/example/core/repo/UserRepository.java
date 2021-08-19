46ecbca7ec5ebb2f689bffb98e2c1a32acb3fd2f
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-08-19 10:56:33 +0900

    drop unused import

--- core/src/main/java/com/example/core/repo/UserRepository.java
+++ core/src/main/java/com/example/core/repo/UserRepository.java
@@ -4,3 +4,4 @@ import com.example.core.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {}
+// TODO clean this
