51ff577e79e81ce7e906f10a7b10a2222c925277
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-08-19 09:01:41 +0900

    core: organization repository

---
+++ core/src/main/java/com/example/core/repo/OrganizationRepository.java
@@ -0,0 +1,6 @@
+package com.example.core.repo;
+
+import com.example.core.domain.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {}
