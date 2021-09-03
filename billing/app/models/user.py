0445f2927b8aef4b1823fc9488ae0f143b1e7241
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-03 10:00:26 +0900

    billing: add user model

---
+++ billing/app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime
+from sqlalchemy.sql import func
+from app.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), nullable=True)
+    full_name = Column(String(255), nullable=True)
+    is_active = Column(Boolean)
+    hashed_password = Column(String(255), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
