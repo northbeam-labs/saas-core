d4ed652fce73a4a73841c83518adc1978a5052a1
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-03 22:27:39 +0900

    web: user api

---
+++ app/src/features/user/api.ts
@@ -0,0 +1,6 @@
+import { api } from "../../lib/api";
+import { User } from "./types";
+
+export const listUsers = () => api.get<User[]>("/users").then((r) => r.data);
+export const getUser = (id: number) => api.get<User>(`/users/${id}`).then((r) => r.data);
+export const createUser = (body: Partial<User>) => api.post<User>("/users", body).then((r) => r.data);
