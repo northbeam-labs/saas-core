1e12b78eb785a446b61c618c1b00cac5c4d2bb81
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-17 17:36:59 +0900

    web: useUsers hook

---
+++ app/src/features/user/useUsers.ts
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import { User } from "./types";
+import { listUsers } from "./api";
+
+export function useUsers() {
+  const [items, setItems] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => { listUsers().then(setItems).finally(() => setLoading(false)); }, []);
+  return { items, loading };
+}
