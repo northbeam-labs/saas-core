82ebd130304c230686bcb5d3e04cad6c30248938
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-29 18:48:38 +0900

    core: project controller

---
+++ core/src/main/java/com/example/core/web/ProjectController.java
@@ -0,0 +1,16 @@
+package com.example.core.web;
+
+import com.example.core.domain.Project;
+import com.example.core.service.ProjectService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+    private final ProjectService service;
+    public ProjectController(ProjectService service) { this.service = service; }
+    @GetMapping public List<Project> all() { return service.all(); }
+    @GetMapping("/{id}") public Project get(@PathVariable Long id) { return service.get(id); }
+    @PostMapping public Project create(@RequestBody Project e) { return service.save(e); }
+}
