1e8fc7f4b21935819979ba690d57e15d52b78c0c
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-09-29 09:34:55 +0900

    readme

--- app/src/features/user/api.ts
+++ app/src/features/user/api.ts
@@ -4,3 +4,4 @@ import { User } from "./types";
 export const listUsers = () => api.get<User[]>("/users").then((r) => r.data);
 export const getUser = (id: number) => api.get<User>(`/users/${id}`).then((r) => r.data);
 export const createUser = (body: Partial<User>) => api.post<User>("/users", body).then((r) => r.data);
+// tidy up
