3a5b9c28e1cdc14a2d9e860ddfaedf1a80d75ac1
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-10-05 15:51:56 +0900

    billing: test user

---
+++ billing/tests/test_user.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_list_users():
+    assert client.get("/users").status_code in (200, 422)
