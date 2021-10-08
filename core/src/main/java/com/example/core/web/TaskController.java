f44524ee2fe503b629611c59212ba61205ee9e2e
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-10-08 13:08:23 +0900

    core: task controller

---
+++ core/src/main/java/com/example/core/web/TaskController.java
@@ -0,0 +1,16 @@
+package com.example.core.web;
+
+import com.example.core.domain.Task;
+import com.example.core.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService service;
+    public TaskController(TaskService service) { this.service = service; }
+    @GetMapping public List<Task> all() { return service.all(); }
+    @GetMapping("/{id}") public Task get(@PathVariable Long id) { return service.get(id); }
+    @PostMapping public Task create(@RequestBody Task e) { return service.save(e); }
+}
