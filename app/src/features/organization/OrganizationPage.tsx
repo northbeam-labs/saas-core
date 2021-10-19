977b680f02c9a452849cf1961250f39401b3f13b
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-10-19 15:26:42 +0900

    web: Organization page

---
+++ app/src/features/organization/OrganizationPage.tsx
@@ -0,0 +1,10 @@
+import OrganizationList from "./OrganizationList";
+
+export default function OrganizationPage() {
+  return (
+    <section>
+      <h2>Organizations</h2>
+      <OrganizationList />
+    </section>
+  );
+}
