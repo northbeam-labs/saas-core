6a3f96dc86ffc4103b4153cc3171bc0be5490145
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-10-20 09:23:03 +0900

    billing: organization service

---
+++ billing/app/services/organization.py
@@ -0,0 +1,20 @@
+from app.repositories.organization import OrganizationRepository
+from app.models.organization import Organization
+from app.exceptions import AppError
+
+
+class OrganizationService:
+    def __init__(self, db):
+        self.repo = OrganizationRepository(db)
+
+    def get(self, id):
+        obj = self.repo.get(id)
+        if not obj:
+            raise AppError(404, "organization not found")
+        return obj
+
+    def list(self, p):
+        return self.repo.list(p.limit, p.offset)
+
+    def create(self, data):
+        return self.repo.create(Organization(**data.dict()))
