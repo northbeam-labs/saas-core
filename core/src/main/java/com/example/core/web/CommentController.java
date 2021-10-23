6f2270a22c79648e87d70b078b3a723b07e60ab1
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-10-23 21:52:31 +0900

    core: comment controller

---
+++ core/src/main/java/com/example/core/web/CommentController.java
@@ -0,0 +1,16 @@
+package com.example.core.web;
+
+import com.example.core.domain.Comment;
+import com.example.core.service.CommentService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private final CommentService service;
+    public CommentController(CommentService service) { this.service = service; }
+    @GetMapping public List<Comment> all() { return service.all(); }
+    @GetMapping("/{id}") public Comment get(@PathVariable Long id) { return service.get(id); }
+    @PostMapping public Comment create(@RequestBody Comment e) { return service.save(e); }
+}
