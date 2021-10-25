3c75b2fc640f603aec2de21856bd4901a4623ba7
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-10-25 16:19:27 +0900

    core: tag entity

---
+++ core/src/main/java/com/example/core/domain/Tag.java
@@ -0,0 +1,18 @@
+package com.example.core.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String label;
+    private String color;
+
+    public Long getId() { return id; }
+    public String getLabel() { return label; }
+    public void setLabel(String v) { this.label = v; }
+    public String getColor() { return color; }
+    public void setColor(String v) { this.color = v; }
+}
