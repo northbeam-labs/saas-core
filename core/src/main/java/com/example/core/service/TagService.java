f233488fd8ede0a1942255f32d4fb7acab27a03a
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-11-04 19:55:09 +0900

    core: tag service

---
+++ core/src/main/java/com/example/core/service/TagService.java
@@ -0,0 +1,15 @@
+package com.example.core.service;
+
+import com.example.core.domain.Tag;
+import com.example.core.repo.TagRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TagService {
+    private final TagRepository repo;
+    public TagService(TagRepository repo) { this.repo = repo; }
+    public List<Tag> all() { return repo.findAll(); }
+    public Tag get(Long id) { return repo.findById(id).orElseThrow(); }
+    public Tag save(Tag e) { return repo.save(e); }
+}
