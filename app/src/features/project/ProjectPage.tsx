9828e9bd3094fe580a0eb9e82df6a668e9160bae
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-11-08 16:11:53 +0900

    web: Project page

---
+++ app/src/features/project/ProjectPage.tsx
@@ -0,0 +1,10 @@
+import ProjectList from "./ProjectList";
+
+export default function ProjectPage() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ProjectList />
+    </section>
+  );
+}
