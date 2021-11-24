dc2b9ce2ea19537589f7a69c429fc0b3c3dd9fb7
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-11-24 16:49:51 +0900

    update readme

--- billing/app/models/user.py
+++ billing/app/models/user.py
@@ -13,3 +13,4 @@ class User(Base):
     hashed_password = Column(String(255), nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+# TODO clean this
