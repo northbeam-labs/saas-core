22b416348d682d118365a03365bbbfc674bd1a18
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-12-05 18:55:54 +0900

    billing: test project

---
+++ billing/tests/test_project.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_list_projects():
+    assert client.get("/projects").status_code in (200, 422)
