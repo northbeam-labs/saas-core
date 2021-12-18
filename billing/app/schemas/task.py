8003b412cb97bf53737a95f45a533422f872dad8
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2021-12-18 21:17:24 +0900

    billing: task schemas

---
+++ billing/app/schemas/task.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class TaskIn(BaseModel):
+    title: str
+    priority: int
+    done: bool
+    due_date: str
+
+
+class TaskOut(TaskIn):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    priority: int | None = None
+    done: bool | None = None
+    due_date: str | None = None
