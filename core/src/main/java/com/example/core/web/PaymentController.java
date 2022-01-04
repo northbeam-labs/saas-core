33200fc8624c9c5ecbcb0f00d7f2a3ed8350afa0
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-01-04 13:47:58 +0900

    core: payment controller

---
+++ core/src/main/java/com/example/core/web/PaymentController.java
@@ -0,0 +1,16 @@
+package com.example.core.web;
+
+import com.example.core.domain.Payment;
+import com.example.core.service.PaymentService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+    private final PaymentService service;
+    public PaymentController(PaymentService service) { this.service = service; }
+    @GetMapping public List<Payment> all() { return service.all(); }
+    @GetMapping("/{id}") public Payment get(@PathVariable Long id) { return service.get(id); }
+    @PostMapping public Payment create(@RequestBody Payment e) { return service.save(e); }
+}
