bcec3a621899125c2afc8d21b73e1a98c409dfb2
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-01-06 16:05:38 +0900

    pin versions

--- core/src/main/java/com/example/core/repo/OrganizationRepository.java
+++ core/src/main/java/com/example/core/repo/OrganizationRepository.java
@@ -4,3 +4,4 @@ import com.example.core.domain.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {}
+// left a note for myself
