82c87e71326b6e40bd66277cc332fddb40accdd5
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-01-12 10:18:36 +0900

    web: Comment page

---
+++ app/src/features/comment/CommentPage.tsx
@@ -0,0 +1,10 @@
+import CommentList from "./CommentList";
+
+export default function CommentPage() {
+  return (
+    <section>
+      <h2>Comments</h2>
+      <CommentList />
+    </section>
+  );
+}
