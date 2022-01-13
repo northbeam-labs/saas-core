ccb04fff50177ec57b8ea13ee4a8e7887c3cd890
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-01-13 12:59:22 +0900

    typo

--- core/src/main/java/com/example/core/web/ProjectController.java
+++ core/src/main/java/com/example/core/web/ProjectController.java
@@ -14,3 +14,4 @@ public class ProjectController {
     @GetMapping("/{id}") public Project get(@PathVariable Long id) { return service.get(id); }
     @PostMapping public Project create(@RequestBody Project e) { return service.save(e); }
 }
+// tidy up
