e245590dedbfcc9d8a032b33bd3b331a6725792a
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-18 16:58:22 +0900

    web: Invoice page

---
+++ app/src/features/invoice/InvoicePage.tsx
@@ -0,0 +1,10 @@
+import InvoiceList from "./InvoiceList";
+
+export default function InvoicePage() {
+  return (
+    <section>
+      <h2>Invoices</h2>
+      <InvoiceList />
+    </section>
+  );
+}
