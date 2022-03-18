6e4e71bf21b33cbab36c9c49532d744dea83c3bd
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-18 10:59:03 +0900

    billing: tag routes

---
+++ billing/app/routers/tag.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.deps import get_db, Pagination
+from app.services.tag import TagService
+from app.schemas.tag import TagIn, TagOut
+
+router = APIRouter(prefix="/tags", tags=["tags"])
+
+
+@router.get("", response_model=list[TagOut])
+def list_tags(p: Pagination = Depends(), db: Session = Depends(get_db)):
+    return TagService(db).list(p)
+
+
+@router.get("/{id}", response_model=TagOut)
+def get_tag(id: int, db: Session = Depends(get_db)):
+    return TagService(db).get(id)
+
+
+@router.post("", response_model=TagOut)
+def create_tag(data: TagIn, db: Session = Depends(get_db)):
+    return TagService(db).create(data)
