9edcf9e76bce17bf19f53fc2a9497b35e47acabe
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-22 14:29:48 +0900

    core: audit_log controller

---
+++ core/src/main/java/com/example/core/web/AuditLogController.java
@@ -0,0 +1,16 @@
+package com.example.core.web;
+
+import com.example.core.domain.AuditLog;
+import com.example.core.service.AuditLogService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/audit_logs")
+public class AuditLogController {
+    private final AuditLogService service;
+    public AuditLogController(AuditLogService service) { this.service = service; }
+    @GetMapping public List<AuditLog> all() { return service.all(); }
+    @GetMapping("/{id}") public AuditLog get(@PathVariable Long id) { return service.get(id); }
+    @PostMapping public AuditLog create(@RequestBody AuditLog e) { return service.save(e); }
+}
