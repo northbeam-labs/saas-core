36d1fc84df1a04192dd65aebc52a59219638f34e
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-29 15:23:22 +0900

    billing: invoice schemas

---
+++ billing/app/schemas/invoice.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class InvoiceIn(BaseModel):
+    number: str
+    amount: float
+    currency: str
+    paid: bool
+
+
+class InvoiceOut(InvoiceIn):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class InvoiceUpdate(BaseModel):
+    number: str | None = None
+    amount: float | None = None
+    currency: str | None = None
+    paid: bool | None = None
