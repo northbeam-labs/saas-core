bec4ac8c7c7ffc18f2f8e9a295123c78c2dafa57
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-03-31 20:13:02 +0900

    web: Payment page

---
+++ app/src/features/payment/PaymentPage.tsx
@@ -0,0 +1,10 @@
+import PaymentList from "./PaymentList";
+
+export default function PaymentPage() {
+  return (
+    <section>
+      <h2>Payments</h2>
+      <PaymentList />
+    </section>
+  );
+}
