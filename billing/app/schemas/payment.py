31f1e72aed79f1a8fc3ee6b6d2ea250cc54835cc
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-04-06 18:30:15 +0900

    billing: payment schemas

---
+++ billing/app/schemas/payment.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+
+
+class PaymentIn(BaseModel):
+    amount: float
+    provider: str
+    status: str
+    reference: str
+
+
+class PaymentOut(PaymentIn):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class PaymentUpdate(BaseModel):
+    amount: float | None = None
+    provider: str | None = None
+    status: str | None = None
+    reference: str | None = None
