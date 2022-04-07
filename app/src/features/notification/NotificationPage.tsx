9cbd4b7574874e77873ada13e39434aed578366c
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-04-07 16:31:24 +0900

    web: Notification page

---
+++ app/src/features/notification/NotificationPage.tsx
@@ -0,0 +1,10 @@
+import NotificationList from "./NotificationList";
+
+export default function NotificationPage() {
+  return (
+    <section>
+      <h2>Notifications</h2>
+      <NotificationList />
+    </section>
+  );
+}
