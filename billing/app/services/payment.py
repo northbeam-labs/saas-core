199998c567f0f78f2a9756eab78848e8d4d6c5f1
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-04-07 18:25:26 +0900

    billing: payment service

---
+++ billing/app/services/payment.py
@@ -0,0 +1,20 @@
+from app.repositories.payment import PaymentRepository
+from app.models.payment import Payment
+from app.exceptions import AppError
+
+
+class PaymentService:
+    def __init__(self, db):
+        self.repo = PaymentRepository(db)
+
+    def get(self, id):
+        obj = self.repo.get(id)
+        if not obj:
+            raise AppError(404, "payment not found")
+        return obj
+
+    def list(self, p):
+        return self.repo.list(p.limit, p.offset)
+
+    def create(self, data):
+        return self.repo.create(Payment(**data.dict()))
