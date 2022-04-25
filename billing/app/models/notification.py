ee42427de058fef0fa99a4a852a244dd33161672
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-04-25 09:50:47 +0900

    billing: add notification model

---
+++ billing/app/models/notification.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime
+from sqlalchemy.sql import func
+from app.db import Base
+
+
+class Notification(Base):
+    __tablename__ = "notifications"
+
+    id = Column(Integer, primary_key=True, index=True)
+    kind = Column(String(255), nullable=True)
+    message = Column(String(255), nullable=True)
+    read = Column(Boolean)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
