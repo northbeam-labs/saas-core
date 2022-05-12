8153e001f3c44293323843bd4004549dd843d2ca
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-05-12 17:44:33 +0900

    web: Webhook page

---
+++ app/src/features/webhook/WebhookPage.tsx
@@ -0,0 +1,10 @@
+import WebhookList from "./WebhookList";
+
+export default function WebhookPage() {
+  return (
+    <section>
+      <h2>Webhooks</h2>
+      <WebhookList />
+    </section>
+  );
+}
