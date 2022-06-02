d56b7d34c0ab5ef09ef9a88e55cd6c016bd2febf
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-06-02 15:07:12 +0900

    billing: test webhook

---
+++ billing/tests/test_webhook.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_list_webhooks():
+    assert client.get("/webhooks").status_code in (200, 422)
