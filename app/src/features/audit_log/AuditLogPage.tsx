04e020d88a9d1873e356973492ae7aeeb9efc5b8
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-06-04 20:47:38 +0900

    web: AuditLog page

---
+++ app/src/features/audit_log/AuditLogPage.tsx
@@ -0,0 +1,10 @@
+import AuditLogList from "./AuditLogList";
+
+export default function AuditLogPage() {
+  return (
+    <section>
+      <h2>AuditLogs</h2>
+      <AuditLogList />
+    </section>
+  );
+}
